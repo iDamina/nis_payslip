ce7260ed200c97a69e4eead1ad91b44a85f008a1
iDamina <daminailiya@gmail.com>
2025-07-23 21:18:50 +0100

    final copy for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
   },
   server: {
     proxy: {
